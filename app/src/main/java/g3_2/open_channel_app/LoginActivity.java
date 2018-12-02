e66cb20b13b71d01c6e93f1d4cd187b91ee4dcdb
KhorZL <14sczile@gmail.com>
2018-12-02 21:46:13 +0800

    Added firebase steps

--- app/src/main/java/g3_2/open_channel_app/LoginActivity.java
+++ app/src/main/java/g3_2/open_channel_app/LoginActivity.java
@@ -1,9 +1,25 @@
 package g3_2.open_channel_app;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LoginActivity extends AppCompatActivity{
+    private String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +31,7 @@ public class LoginActivity extends AppCompatActivity {
                     .replace(R.id.container, LoginFragment.newInstance())
                     .commitNow();
         }
+        //getData("user/johnny");
     }
+
 }
