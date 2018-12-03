0a88a4c2e199563462c0f18b8f8bc9c16b7056d0
herringblue <ivanckj96@gmail.com>
2018-12-03 22:00:21 +0800

    connected profile to firestore

---
+++ app/src/main/java/g3_2/open_channel_app/profile/ProfileEntry.java
@@ -0,0 +1,31 @@
+package g3_2.open_channel_app.network;
+
+import java.util.List;
+
+/**
+ * A product entry in the list of products.
+ */
+public class ProfileEntry {
+    private static final String TAG = ProfileEntry.class.getSimpleName();
+
+    public final String title;
+    public final String url;
+    public final String organisation;
+    public final String description;
+    public final List subscribers;
+    public final String id;
+
+    public ProfileEntry(
+            String title, String url, String organisation, String description, List subscribers, String id) {
+        this.title = title;
+        this.url = url;
+        this.organisation = organisation;
+        this.description = description;
+        this.subscribers = subscribers;
+        this.id = id;
+    }
+
+    /**
+     * Loads a raw JSON at R.raw.products and converts it into a list of ProductEntry objects
+     */
+}
---
+++ app/src/main/java/g3_2/open_channel_app/profile/ProfileViewHolder.java
@@ -0,0 +1,37 @@
+package g3_2.open_channel_app.channels;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import g3_2.open_channel_app.R;
+import g3_2.open_channel_app.network.ChannelEntry;
+
+public class CardChannelViewHolder extends RecyclerView.ViewHolder {
+
+    public NetworkImageView channelImage;
+    public TextView channelTitle;
+    public TextView channelOrganisation;
+    public TextView channelDescription;
+    public Button button;
+    public ChannelEntry currentItem;
+
+
+    public CardChannelViewHolder(@NonNull View itemView, Fragment fragment) {
+        super(itemView);
+        channelImage = itemView.findViewById(R.id.channelImage);
+        channelTitle = itemView.findViewById(R.id.title);
+        channelOrganisation = itemView.findViewById(R.id.organisation);
+        channelDescription = itemView.findViewById(R.id.description);
+        button= (Button) itemView.findViewById(R.id.subscribe_button);
+
+    }
+}
